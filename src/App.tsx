1f24155a228db99da52f28c471b123b7a65c7191
SQflex <egorkazgorki2@gmail.com>
2022-06-01 15:36:15 +0300

    add: lost figures counter with name and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,57 @@
+import { title } from 'process';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Figure } from './models/figures/Figure';
+import { Player } from './models/Player';
 
 function App() {
   const [board, setBoard] = useState(new Board());
-
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
   useEffect(()=>{
     restart();
+    setCurrentPlayer(whitePlayer);
   }, []);
 
-  function restart(){
+  function restart() {
     const newBoard = new Board();
     newBoard.initCells()
     newBoard.addFigures()
     setBoard(newBoard)
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+  }
+  
   return (
-    <div className="App">
+    <div className="app">
+      <Timer restart={restart}
+      currentPlayer={currentPlayer}
+      />
       <BoardComponent 
       board={board}
       setBoard={setBoard}
+      currentPlayer={currentPlayer}
+      swapPlayer={swapPlayer}
       />
-    </div>
+      <div>
+        <LostFigures
+          title="Черные фигуры"
+          figures={board.lostBlackFigures}
+        />
+        <LostFigures
+          title="Белые фигуры"
+          figures={board.lostWhiteFigures}
+        />
+      </div>
+      </div>
   );
 };
 
